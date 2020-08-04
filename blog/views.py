fadd42e4c1ecc41dc68667eaaba851f674b7ded0
logiqueviper <kanadeanand67@gmail.com>
2020-08-04 12:58:05 +0530

    first update

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-posts = [int(i) for i in range(1,21)]
 def home(request):
-    context = {
-        'posts':posts
-    }
-    return render(request,'blog/home.html',context)
+    return render(request,'blog/home.html')
 def about(request):
     return render(request,'blog/about.html')
 def option(request):
